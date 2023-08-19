c7c1a5dd12bc4d46e1b3e0cb98b7a726cb06ffe7
Robson Amorim <robson.amorim@lambad3.com.br>
2023-08-19 12:50:09 -0300

    Initial Commit

---
+++ hello_ios/hello_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_iosApp.swift
+//  hello_ios
+//
+//  Created by Robson Amorim on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct hello_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
